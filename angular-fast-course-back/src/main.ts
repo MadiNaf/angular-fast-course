ea719ad371e7a3805eced1bd0dfa41927e630afc
MadiBlh <madiats2@gmail.com>
2022-10-25 14:45:24 +0200

    add aws

--- angular-fast-course-back/src/main.ts
+++ angular-fast-course-back/src/main.ts
@@ -17,7 +17,8 @@ async function bootstrap() {
   app.enableCors(
     // { origin: origin, methods: 'GET,PUT,POST,UPDATE,DELETE', credentials: true }
   );
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
 initChatDataBase();
